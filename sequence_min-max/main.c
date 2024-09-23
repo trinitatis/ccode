f95a12327b9de0744e5c98da4f4017c745c4909f
gurskiy.a <gurskiy.a@hotmail.com>
2024-09-23 11:26:35 +0300

    develop:sequence_min-max:input to function

--- sequence_min-max/main.c
+++ sequence_min-max/main.c
@@ -3,25 +3,34 @@
 
 #define CHUNK_SIZE 8
 
+void input(int* seq, int* error);
+
 int main(void) {
     int error = 0;
-    int item = 0;
-    int idx = 0;
-    char ch = 0;
     int* seq;
     
     seq = malloc(sizeof(int) * CHUNK_SIZE);
 
-    while ( item != -1 ) {
-        if ( scanf("%d%c", &item, &ch) != 2 || (item != -1 && ch != ' ') ) {
-            error = 1;
-            break;
-        }
-        seq[idx++] = item;
-    }
+    input(seq, &error);
+    
     if (error) printf("ERROR\n");
        
     free(seq);
 
     return 0;
+}
+
+
+void input(int* seq, int* error) {
+    int item = 0;
+    int idx = 0;
+    char ch = 0;
+
+    while ( item != -1 ) {
+        if ( scanf("%d%c", &item, &ch) != 2 || (item != -1 && ch != ' ') ) {
+            *error = 1;
+            break;
+        }
+        seq[idx++] = item;
+    }
 }

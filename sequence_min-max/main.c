b54e0112fb039714609b44c57c7de644485efe60
gurskiy.a <gurskiy.a@hotmail.com>
2024-09-23 17:31:13 +0300

    develop:NOTHING yet

--- sequence_min-max/main.c
+++ sequence_min-max/main.c
@@ -1,31 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define CHUNK_SIZE 8
+#define CHUNK_SIZE 4
 
-void input(int* seq, int* error);
+void input(int* seq, int* len, int* error);
 void output(int* seq);
 
 int main(void) {
     int error = 0;
-    int* seq = malloc(sizeof(int) * CHUNK_SIZE);
-    int len = sizeof(*seq) / sizeof(int);
+    int len = CHUNK_SIZE;
+    int* seq = (int*)malloc(sizeof(int) * len);
 
-    printf("len: %d\n", len);
-
-    input(seq, &error);
+    input(seq, &len, &error);
     if (error) {
         printf("ERROR\n");
     } else {
         output(seq);
     }
       
-    free(seq);
+    //free(seq);
 
     return 0;
 }
 
-void input(int* seq, int* error) {
+void input(int* seq, int* len, int* error) {
     int item = 0;
     int idx = 0;
     char ch = 0;
@@ -35,8 +33,20 @@ void input(int* seq, int* error) {
             *error = 1;  // bad input
             break;
         }
-        seq[idx++] = item;
+        printf("idx:%d, len:%d\n", idx, *len);
+        seq[idx] = item;
+        if (idx + 1 > *len) {
+            *len += CHUNK_SIZE;
+            if  ( (seq = realloc(seq, *len * sizeof(int))) ) {
+                printf("seq[%d]==%d\n", idx, seq[idx]);
+            } else {
+                *error = 1;
+                break;
+            }
+        }
+        idx++;
     }
+ 
 }
 
 void output(int* seq) {

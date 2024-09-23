e9d2acaac6b0e660b8e40e361fae4b430e998721
gurskiy.a <gurskiy.a@hotmail.com>
2024-09-23 23:29:45 +0300

    develop:test realloc()

--- sequence_min-max/test_realloc_bad.c
+++ sequence_min-max/test_realloc_bad.c
@@ -28,15 +28,17 @@ void input(int* seq) {
     char ch = 0;
     int item = 0;
     int idx = 0;
+    int* new = NULL;
 
     while (item != -1) {
         scanf("%d%c", &item, &ch);
         seq[idx] = item;
         if (idx + 1 == size) {
             size++;
-            seq = realloc(seq, size);
+            new = (int*)realloc(seq, sizeof(seq) + sizeof(int));
             /* For test */
-            printf("idx:%d:", idx); for (int i = 0; i <= idx; i++) printf("%d", seq[i]); printf("\n");
+            printf("idx:%d size:%d sizeof:%d\t", idx, size, (int)sizeof(new)); for (int i = 0; i <= idx; i++) printf("%d", seq[i]); printf("\n");
+            seq = new;
         }
         idx++;
     }

7d6695b72ff14a6003e788bc2f8c87352c20b2bc
gurskiy.a <gurskiy.a@hotmail.com>
2024-09-23 14:20:00 +0300

    develop:reset

--- sequence_min-max/main.c
+++ sequence_min-max/main.c
@@ -3,44 +3,62 @@
 
 #define CHUNK_SIZE 8
 
-void input(int* seq, int* error);
-void output(int* seq);
+struct Sequence {
+    int len;
+    int capacity;
+    int data[0];
+};
+
+void input(struct Sequence* seq, int* error);
+void output(struct Sequence* seq);
 
 int main(void) {
     int error = 0;
-    int* seq;
-    
-    seq = malloc(sizeof(int) * CHUNK_SIZE);
+    struct Sequence* seq = malloc(sizeof(struct Sequence) + sizeof(int) * CHUNK_SIZE);
+    seq->len = 0;
+    seq->capacity = CHUNK_SIZE;
 
     input(seq, &error);
-    output(seq);
-    
-    if (error) printf("ERROR\n");
-       
+    if (error) {
+        printf("ERROR\n");
+    } else {
+        output(seq);
+    }
+      
     free(seq);
 
     return 0;
 }
 
-void input(int* seq, int* error) {
-    int* ptr = seq;
+void input(struct Sequence* seq, int* error) {
     int item = 0;
+    int idx = 0;
     char ch = 0;
+    struct Sequence* new;
 
     while ( item != -1 ) {
         if ( scanf("%d%c", &item, &ch) != 2 || (item != -1 && ch != ' ') ) {
-            *error = 1;
+            *error = 1;  // bad input
             break;
         }
-        *ptr = item;
-        ptr++;
+        if (seq->len + 1 > seq->capacity) {
+            seq->capacity = seq->capacity + CHUNK_SIZE;
+            new = realloc(seq, sizeof(struct Sequence) + sizeof(int) * seq->capacity);
+            if (new == NULL) {
+                *error = 1;  // out of memory
+                break;
+            }
+            seq = new;
+        }
+        seq->data[idx++] = item;
+        seq->len++;
     }
 }
 
-void output(int* seq) {
+void output(struct Sequence* seq) {
 
-    while (*(seq) != -1) {
-        printf("%d ", *seq);
-        seq++;
+    for (int* ptr = seq->data; *ptr != -1; ptr++) {
+        printf("%d ", *ptr);
     }
+    printf("\nlen: %d; size: %d", seq->len, seq->capacity);
 }
